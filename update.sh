4b5a5dc563a9e467a9fc4405c4be5ae5f4f91473
Greg Hogan <gregjhogan@gmail.com>
2021-06-08 17:13:09 -0700

    update nightly

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 
 cd "$(dirname "$0")"
 
-lftp -d -u anonymous, -e 'set ftp:ssl-force true' -e 'mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/1[8-9][0-9][0-9][0-9]/ --directory=/MCC/PRODUCTS/[2-9][0-9][0-9][0-9][0-9]/ --include-glob=final/Sta[0-9][0-9][0-9][0-9][0-9].sp3 --include-glob=rapid/Sta[0-9][0-9][0-9][0-9][0-9].sp3 --include-glob=ultra/Sta[0-9][0-9][0-9][0-9][0-9].sp3 --target-directory=./MCC/PRODUCTS/;exit' ftp.glonass-iac.ru
-
+DOY="[0-9][0-9][0-9]"
+DOW="[0-9]"
+GPS_WEEK="[0-9][0-9][0-9][0-9]"
+lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/2[1-9]${DOY}/ --directory=/MCC/PRODUCTS/[3-9][0-9]${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
