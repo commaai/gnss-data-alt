068ce2879e975db243aa2a5cc336c43cca5144b2
Greg Hogan <gregjhogan@gmail.com>
2021-09-17 11:05:26 -0700

    update files that change

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 DOY="[0-9][0-9][0-9]"
 DOW="[0-9]"
 GPS_WEEK="[0-9][0-9][0-9][0-9]"
-lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/2[1-9]${DOY}/ --directory=/MCC/PRODUCTS/[3-9][0-9]${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/2[1-9]${DOY}/ --directory=/MCC/PRODUCTS/[3-9][0-9]${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
