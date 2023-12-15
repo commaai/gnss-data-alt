cf331f4085f4c1f697a4c3f4487df8ced57b3747
Greg Hogan <gregjhogan@gmail.com>
2023-12-15 11:24:37 -0800

    one year at a time

--- update.sh
+++ update.sh
@@ -9,7 +9,13 @@ DOY="[0-9][0-9][0-9]"
 DOW="[0-9]"
 GPS_WEEK="[0-9][0-9][0-9][0-9]"
 
-CURR_YEAR=$(date -u +%y)
-PREV_YEAR=$(( ${CURR_YEAR} - 1 ))
+END_YEAR=$(date -u +%y)
+START_YEAR=$(( ${END_YEAR} - 1 ))
 
-lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10 --directory=/MCC/PRODUCTS/${CURR_YEAR}${DOY}/ --directory=/MCC/PRODUCTS/${PREV_YEAR}${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+LFTP_MIRROR_OPTS="--no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10"
+
+for YEAR in $(seq ${START_YEAR} ${END_YEAR}); do
+  DIR_PATH="/MCC/PRODUCTS/${YEAR}${DOY}/"
+  echo "STARTING: ${DIR_PATH}"
+  lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror ${LFTP_MIRROR_OPTS} --directory=${DIR_PATH} --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+done
