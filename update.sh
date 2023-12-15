3fdb1bec9da166e629cbae25915a296658c4b470
Greg Hogan <gregjhogan@gmail.com>
2023-12-15 11:03:16 -0800

    ignore time to avoid always downloading every file

--- update.sh
+++ update.sh
@@ -12,4 +12,4 @@ GPS_WEEK="[0-9][0-9][0-9][0-9]"
 CURR_YEAR=$(date -u +%y)
 PREV_YEAR=$(( ${CURR_YEAR} - 1 ))
 
-lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/${CURR_YEAR}${DOY}/ --directory=/MCC/PRODUCTS/${PREV_YEAR}${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10 --directory=/MCC/PRODUCTS/${CURR_YEAR}${DOY}/ --directory=/MCC/PRODUCTS/${PREV_YEAR}${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
