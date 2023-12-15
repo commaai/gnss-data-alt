23ab02c4ac24205a14bdc8f533d155b6d8e6540e
Greg Hogan <gregjhogan@gmail.com>
2023-12-15 13:41:22 -0800

    lftp reusable function

--- update.sh
+++ update.sh
@@ -1,4 +1,29 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+function sync_dir
+{
+  if [ -z $1 ]; then
+    echo "ERROR: target directory value is not set"
+    exit 1
+  fi
+  TARGET_DIR="$1"
+
+  if [ -z $2 ]; then
+    echo "ERROR: source directory array is not set"
+    exit 1
+  fi
+  SOURCE_DIRS=($2) # convert space separated string to array
+
+  if [ -z $3 ]; then
+    echo "ERROR: include glob array is not set"
+    exit 1
+  fi
+  INCLUDE_GLOBS=($3) # convert space separated string to array
+
+  SERVER_HOSTNAME="ftp.glonass-iac.ru"
+  LFTP_MIRROR_OPTS="--no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10"
+  lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror ${LFTP_MIRROR_OPTS} $(echo ${SOURCE_DIRS[@]/#/--directory=}) $(echo ${INCLUDE_GLOBS[@]/#/--include-glob=}) --target-directory=${TARGET_DIR};exit" ${SERVER_HOSTNAME}
+}
 
 cd "$(dirname "$0")"
 HH="[0-2][0-9]"
@@ -14,8 +39,8 @@ START_YEAR=$(( ${END_YEAR} - 1 ))
 
 LFTP_MIRROR_OPTS="--no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10"
 
-for YEAR in $(seq ${START_YEAR} ${END_YEAR}); do
-  DIR_PATH="/MCC/PRODUCTS/${YEAR}${DOY}/"
-  echo "STARTING: ${DIR_PATH}"
-  lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror ${LFTP_MIRROR_OPTS} --directory=${DIR_PATH} --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+for CURR_YEAR in $(seq ${START_YEAR} ${END_YEAR}); do
+  echo "STARTING YEAR: ${CURR_YEAR}"
+  #        target dir         source dirs (space separated array)   include globs (space separated array)
+  sync_dir "./MCC/PRODUCTS/"  "/MCC/PRODUCTS/${CURR_YEAR}${DOY}/"   "final/Sta${GPS_WEEK}${DOW}.sp3 rapid/Sta${GPS_WEEK}${DOW}.sp3 ultra/Sta${GPS_WEEK}${DOW}.sp3 ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3"
 done
