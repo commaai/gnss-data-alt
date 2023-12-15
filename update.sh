bbe6f9311406faeb95d1ab2e7a892ac86d0370ea
Greg Hogan <gregjhogan@gmail.com>
2023-12-15 10:15:20 -0800

    restrict update time range based on current date

--- update.sh
+++ update.sh
@@ -9,4 +9,7 @@ DOY="[0-9][0-9][0-9]"
 DOW="[0-9]"
 GPS_WEEK="[0-9][0-9][0-9][0-9]"
 
-lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/2[1-9]${DOY}/ --directory=/MCC/PRODUCTS/[3-9][0-9]${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
+CURR_YEAR=$(date -u +%y)
+PREV_YEAR=$(( ${CURR_YEAR} - 1 ))
+
+lftp -d -u anonymous, -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/MCC/PRODUCTS/${CURR_YEAR}${DOY}/ --directory=/MCC/PRODUCTS/${PREV_YEAR}${DOY}/ --include-glob=final/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=rapid/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Sta${GPS_WEEK}${DOW}.sp3 --include-glob=ultra/Stark_1D_${YY}${MM}${DD}${HH}.sp3 --target-directory=./MCC/PRODUCTS/;exit" ftp.glonass-iac.ru
